ebda20f815826fed0733d5e3fb626bfcf8b2bde6
Benido <remi.besnie@gmail.com>
2022-05-23 19:39:56 +0200

    Set the tailwing classes for the overall layout. Add dice faces images and the JS logic

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 //DOM elements
-const boxPlayer1 = document.getElementById("boxPlayer1")
-const boxPlayer2 = document.getElementById("boxPlayer2")
-const roundPlayer1 = document.getElementById("roundPlayer1")
-const roundPlayer2 = document.getElementById("roundPlayer2")
-const globalPlayer1 = document.getElementById("globalPlayer1")
-const globalPlayer2 = document.getElementById("globalPlayer2")
-const newGameButton = document.getElementById("newGameButton")
-const diceResult = document.getElementById("dice")
-const rollDiceButton = document.getElementById("rollDiceButton")
-const holdButton = document.getElementById("holdButton")
+const boxPlayer1 = document.getElementById("box-player1")
+const boxPlayer2 = document.getElementById("box-player2")
+const roundPlayer1 = document.getElementById("round-player1")
+const roundPlayer2 = document.getElementById("round-player2")
+const globalPlayer1 = document.getElementById("global-player1")
+const globalPlayer2 = document.getElementById("global-player2")
+const newGameButton = document.getElementById("new-game-button")
+const rollDiceButton = document.getElementById("roll-dice-button")
+const holdButton = document.getElementById("hold-button")
+const diceFaces = document.getElementById("dice").children
 
+
+// global variables
 let round = 0
 let player1 = {string: "Player1", score : 0, box: boxPlayer1, roundDisplay: roundPlayer1, globalDisplay: globalPlayer1}
 let player2 = {string: "Player2", score : 0, box: boxPlayer2, roundDisplay: roundPlayer2, globalDisplay: globalPlayer2}
 let activePlayer = player1
 
-//DOM elements
 
 
+//functions
 function switchPlayer () {
     activePlayer.box.classList.remove("active")
     activePlayer === player1 ? activePlayer = player2 : activePlayer = player1 
@@ -38,13 +40,14 @@ function youWin () {
 }
 
 function newGame () {
+  //set all scores to zero
   round = 0
   player1.score = 0
   player2.score = 0
   refreshDisplay(player1, "roundDisplay", 0)
   refreshDisplay(player2, "roundDisplay", 0)
-  refreshDisplay(player1, "globalDisplay", "")
-  refreshDisplay(player2, "globalDisplay", "")
+  refreshDisplay(player1, "globalDisplay", 0)
+  refreshDisplay(player2, "globalDisplay", 0)
   activePlayer.box.classList.remove("active")
   activePlayer = player1
   activePlayer.box.classList.add("active")
@@ -54,9 +57,18 @@ function getDiceResult () {
   return Math.floor(Math.random() * 6 + 1)
 } 
 
-function throwDice (player) {
+function pickFace (elementCollection, result) {
+  for (let i = 0; i < elementCollection.length; i++) {
+    elementCollection[i].classList.add("hidden")
+  }
+  let newFace = elementCollection[result-1]
+  newFace.classList.remove("hidden")
+}
+
+function rollDice (player) {
   result = getDiceResult()
-  diceResult.innerText = result
+  //diceResult.innerText = result
+  pickFace(diceFaces, result)
   if (result === 1) {
     youLose()
     return 
@@ -86,6 +98,6 @@ function refreshDisplay (player, property, content) {
 
 
 //startGame()
-rollDiceButton.addEventListener("click", ()=>{throwDice(activePlayer)})
+rollDiceButton.addEventListener("click", ()=>{rollDice(activePlayer)})
 holdButton.addEventListener("click", ()=>{hold(activePlayer)})
 newGameButton.addEventListener("click", ()=>{newGame()})
